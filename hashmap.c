dca02bb53f62198d3994b649d40d6e9bf6121c81
NoLeSeAProgramacion <163350378+NoLeSeAProgramacion@users.noreply.github.com>
2024-04-24 20:30:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -141,7 +141,7 @@ Pair * nextMap(HashMap * map)
   
   map -> current = (map -> current+1) % map -> capacity;
   
-  while(map -> current != -1)
+  while(map -> current != map -> capacity)
     {
       if(map -> buckets[map -> current] != NULL && map -> buckets[map -> current] -> key != NULL)
       {

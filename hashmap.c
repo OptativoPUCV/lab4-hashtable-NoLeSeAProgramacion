9af6c4406413d67d7b7ebad57e9120ebda7ba03e
NoLeSeAProgramacion <163350378+NoLeSeAProgramacion@users.noreply.github.com>
2024-04-24 20:09:50 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -39,9 +39,22 @@ int is_equal(void* key1, void* key2){
 }
 
 
-void insertMap(HashMap * map, char * key, void * value) {
-
-
+void insertMap(HashMap * map, char * key, void * value)
+{
+    if(map==NULL || key==NULL) return;
+    if(map->size==map->capacity) enlarge(map);
+    long posicion = hash(key,map->capacity);
+    while(map->buckets[posicion]!=NULL && map->buckets[posicion]->key!=NULL)
+      {
+        if(is_equal(map->buckets[posicion]->key,key))
+        {
+          map->buckets[posicion]->value=value;
+          return;
+        }
+        posicion=(posicion+1)%map->capacity;
+      }
+    map->buckets[posicion]=createPair(key,value);
+    map->size++;
 }
 
 void enlarge(HashMap * map) {
@@ -62,7 +75,9 @@ HashMap * createMap(long capacity)
   
 }
 
-void eraseMap(HashMap * map,  char * key) {    
+void eraseMap(HashMap * map,  char * key)
+{
+  
 
 
 }
